e6a6c44344d9a3a100bcce59265358b3d6879445
Rishabh Mehta <rishabh2605@users.noreply.github.com>
2020-03-30 18:50:57 -0500

    Update preprocessing.py
    reverting previous changes to function preprecess_tokenize

--- preprocessing.py
+++ preprocessing.py
@@ -4,23 +4,25 @@ import urllib
 import re
 import sys
 from bs4 import BeautifulSoup
-from sklearn.feature_extraction.text import TfidfVectorizer
 
-def preprocess(text):
+def preprocess_tokenize(text):
     soup = BeautifulSoup(text,'html.parser')
     text = soup.prettify()
     text = re.sub('<!--.*-->','',text)
     text = re.sub('<[^>]*>','',text)
     text=re.sub('[^0-9a-zA-Z]',' ',text)
     text=re.sub(r"\b[nbrt]\b",' ',text)
-    text = re.sub("\s+",' ',text)
-    return text
+    text = re.sub(r"\s+",' ',text)
+    text=text.lower()
+    return text.split()
+
 
 
 
 data = pd.read_pickle('crawler.pk1')
-data['web_page']=data['web_page'].apply(lambda x:preprocess(x))
+data['web_page']=data['web_page'].apply(lambda x:preprocess_tokenize(x))
 
 
-tfidf = TfidfVectorizer()
-tfidf.fit_transform(data['web_page'])
+
+vectorizer = TfidfVectorizer()
+data_vector=vectorizer.fit_transform(data['web_page'])

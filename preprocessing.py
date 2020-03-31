5697bce32a0588c3c17b80e84b63e1334113aa32
Rishabh Mehta <rishabh2605@users.noreply.github.com>
2020-03-30 23:37:49 -0500

    commit

--- preprocessing.py
+++ preprocessing.py
@@ -17,28 +17,25 @@ def preprocess_tokenize(text):
     text=re.sub(r"\b[nbrt]\b",' ',text)
     text = re.sub(r"\s+",' ',text)
     text=text.lower()
-    text = ' '.join(word for word in text.split() if word not in stopwords.words('english'))
     return text.split()
 
 def generate_inverted_index(df):
     inv = {}
     for i in range(len(df)):
         for word in set(df.tokens[i]):
-            if word not in inv.keys():
-                inv[word] = [[df.index[i],pd.Series(df.tokens[i]).value_counts()[word]]]
-            elif word in inv.keys():
-                inv[word].append([df.index[i],pd.Series(df.tokens[i]).value_counts()[word]])
+            if word not in stopwords.words('english'):
+                if word not in inv.keys():
+                    inv[word] = [[df.index[i],pd.Series(df.tokens[i]).value_counts()[word]]]
+                elif word in inv.keys():
+                    inv[word].append([df.index[i],pd.Series(df.tokens[i]).value_counts()[word]])
     return inv
 
 
 
-import time 
-start = time.time()
+
 print("Pre processing..")
 data = pd.read_pickle('crawler.pk1')
 data['tokens']=data['web_page'].apply(lambda x:preprocess_tokenize(x))
-print(time.time()-start)
-start =time.time()
+
 print("inverted index")
 inverted_index = generate_inverted_index(data)
-print(time.time()-start)
